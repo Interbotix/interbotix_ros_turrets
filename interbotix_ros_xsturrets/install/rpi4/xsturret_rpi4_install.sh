3e5e738ba4c6d5ce041c54795f8bcf0b89c7c14c
Solomon Wiznitzer <shlomo.wiz@gmail.com>
2020-10-26 12:10:06 -0500

    to make it able to run at boot, you need to set hdmi_force_hotplug to 1. otherwise the gui will fail to load.

--- interbotix_ros_xsturrets/install/rpi4/xsturret_rpi4_install.sh
+++ interbotix_ros_xsturrets/install/rpi4/xsturret_rpi4_install.sh
@@ -133,6 +133,7 @@ fi" > xsturret_rpi4_launch.sh
   sudo cp xsturret_rpi4_boot.service /lib/systemd/system/
   sudo systemctl daemon-reload
   sudo systemctl enable xsturret_rpi4_boot.service
+  echo -e "hdmi_force_hotplug=1\nhdmi_group=2\nhdmi_mode=82" | sudo tee -a /boot/firmware/usercfg.txt > /dev/null
 fi
 
 end_time="$(date -u +%s)"
