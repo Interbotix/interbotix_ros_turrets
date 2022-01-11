44f0bafd2899c538511a057071418a6abf72d8f5
Luke Schmitt <luke@trossenrobotics.com>
2022-01-10 18:45:24 -0600

    [python_demos] Modify python demos
    Change instantiation of the Turret object to prevent type errors.
    Make demos easier to read by placing each arg on a single line.

--- interbotix_ros_xsturrets/examples/python_demos/turret_mixed_profile_control.py
+++ interbotix_ros_xsturrets/examples/python_demos/turret_mixed_profile_control.py
@@ -7,22 +7,42 @@ from interbotix_xs_modules.turret import InterbotixTurretXS
 # Note that in general, each 'profile_velocity' unit is 0.229 rev/min and each 'profile_acceleraton' unit is 214.577 rev/min^2 when using the Velocity-based-Profile
 
 def main():
-    # By default, the 'pan' and 'tilt' motors are intialized with the 'time' profile_type. So change to 'velocity' for this demo
-    robot = InterbotixTurretXS(robot_model="pxxls", pan_profile_type="time", tilt_profile_type="velocity")
+    # By default, the 'pan' and 'tilt' motors are intialized with the 'time' profile_type. So change the pan servo to 'velocity' for this demo
+    # The movement of the pan servo should take 1 second and spend half a second accelerating and decelerating
+    # The max velocity of the tilt servo should be about pi/2 rad/s and the max acceleration about 1.87 rad/s^2
+    robot = InterbotixTurretXS(
+        robot_model="pxxls",
+        pan_profile_type="time",
+        tilt_profile_type="velocity",
+        pan_profile_velocity=1.0,
+        pan_profile_acceleration=0.5,
+        tilt_profile_velocity=65,
+        tilt_profile_acceleration=5)
 
-    # move the pan servo to 1.0 radians; make the movement take 1 second and spend half a second accelerating and decelerating;
+    # move the pan servo to 1.0 radians
     # then wait until the motion finishes to return control to the user
-    robot.turret.pan(position=1.0, profile_velocity=1.0, profile_acceleration=0.5, blocking=True)
+    robot.turret.pan(
+        position=1.0,
+        blocking=True)
 
-    # move the tilt servo to 1.5 radians; make the max velocity about pi/2 rad/s and the max acceleration about 1.87 rad/s^2;
+    # move the tilt servo to 1.5 radians
     # then arbitrarily wait 2 seconds to return control to the user
-    robot.turret.tilt(position=1.5, profile_velocity=65, profile_acceleration=5, delay=2.0)
+    robot.turret.tilt(
+        position=1.5,
+        delay=2.0)
 
     # move the turret to [0.8, -1] radians
     # make the pan peak-velocity about 0.75 pi rad/s and the tilt peak-velocity about 0.25 pi rad/s
     # make the pan peak-acceleration about 1.87 rad/s^2 and tilt peak-acceleration 'infinite' (accelerate as fast as physically possible)
     # then arbitrarily wait 3 seconds to return control to the user
-    robot.turret.pan_tilt_move(-0.8, -1, pan_profile_velocity=1.0, pan_profile_acceleration=0.4, tilt_profile_velocity=33, tilt_profile_acceleration=0, delay=3.0)
+    robot.turret.pan_tilt_move(
+        pan_position=-0.8,
+        tilt_position=-1,
+        pan_profile_velocity=1.0,
+        pan_profile_acceleration=0.4,
+        tilt_profile_velocity=33,
+        tilt_profile_acceleration=0,
+        delay=3.0)
 
     # move the turret back home (0 rad for both pan and tilt)
     # use the last 'profile_velocity' and 'profile_acceleration' settings by default (ones from the line above)
--- interbotix_ros_xsturrets/examples/python_demos/turret_velocity_profile_control.py
+++ interbotix_ros_xsturrets/examples/python_demos/turret_velocity_profile_control.py
@@ -8,21 +8,45 @@ from interbotix_xs_modules.turret import InterbotixTurretXS
 
 def main():
     # By default, the 'pan' and 'tilt' motors are intialized with the 'time' profile_type. So change to 'velocity' for this demo
-    robot = InterbotixTurretXS(robot_model="pxxls", pan_profile_type="velocity", tilt_profile_type="velocity")
+    # set the pan servo's max velocity about pi rad/s (equivelant to 131), and the max acceleration about 5.6 rad/s^2 (equivelant to 15)
+    # set the tilt servo's max velocity about pi/2 rad/s and the max acceleration about 1.87 rad/s^2
+    robot = InterbotixTurretXS(
+        robot_model="pxxls",
+        pan_profile_type="velocity",
+        tilt_profile_type="velocity",
+        pan_profile_velocity=131,
+        pan_profile_acceleration=131,
+        tilt_profile_velocity=65,
+        tilt_profile_acceleration=5)
 
-    # move the pan servo to 1.0 radians; make the max velocity about pi rad/s (equivelant to 131), and the max acceleration about 5.6 rad/s^2 (equivelant to 15)
+    # move the pan servo to 1.0 radians
     # then arbitrarily wait 1 second until the motion finishes to return control to the user
-    robot.turret.pan(position=1.0, profile_velocity=131, profile_acceleration=15, delay=1.0)
+    robot.turret.pan(
+        position=1.0,
+        profile_velocity=131,
+        profile_acceleration=15,
+        delay=1.0)
 
-    # move the tilt servo to 1.5 radians; make the max velocity about pi/2 rad/s and the max acceleration about 1.87 rad/s^2;
+    # move the tilt servo to 1.5 radians 
     # then arbitrarily wait 2 seconds to return control to the user
-    robot.turret.tilt(position=1.5, profile_velocity=65, profile_acceleration=5, delay=2.0)
+    robot.turret.tilt(
+        position=1.5,
+        profile_velocity=65,
+        profile_acceleration=5,
+        delay=2.0)
 
     # move the turret to [0.8, -1] radians
     # make the pan peak-velocity about 0.75 pi rad/s and the tilt peak-velocity about 0.25 pi rad/s
     # make the pan peak-acceleration about 1.87 rad/s^2 and tilt peak-acceleration 'infinite' (accelerate as fast as physically possible)
     # then arbitrarily wait 3 seconds to return control to the user
-    robot.turret.pan_tilt_move(-0.8, -1, pan_profile_velocity=98, pan_profile_acceleration=5, tilt_profile_velocity=33, tilt_profile_acceleration=0, delay=3.0)
+    robot.turret.pan_tilt_move(
+        pan_position=-0.8,
+        tilt_position=-1,
+        pan_profile_velocity=98,
+        pan_profile_acceleration=5,
+        tilt_profile_velocity=33,
+        tilt_profile_acceleration=0,
+        delay=3.0)
 
     # move the turret back home (0 rad for both pan and tilt)
     # use the last 'profile_velocity' and 'profile_acceleration' settings by default (ones from the line above)
